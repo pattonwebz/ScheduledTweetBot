cddc9b20855147541859976229e1dc34a611de26
Will <will@pattonwebz.com>
2015-11-12 10:00:33 +0000

    Change the api.update_status() call to explicitly state the 'status' message.
        - A recent version of Tweepy required it to be explicit, no harm in always being so

--- twitterfunctions.py
+++ twitterfunctions.py
@@ -9,20 +9,19 @@ import tweepy
 def authenticatetwitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET):
     # Authenticate with Twitter using keys and secrets and return
     # an 'api' object
-   
-    # Authorize with consumer credentials and get an access token 
+
+    # Authorize with consumer credentials and get an access token
     # with access credentials
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-   
+
     # get an authenticated instance of the API class
     api = tweepy.API(auth)
-    
+
     # return API object 'api'
     return api
-    
+
 
 def sendtweet(api, tweet):
     # Send 'tweet' using Tweepy API function
-    api.update_status(tweet)
-
+    api.update_status(status=tweet)
