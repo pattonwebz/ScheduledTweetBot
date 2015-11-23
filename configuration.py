d5b487cb95099ffa8633c0d6d5a5017205d47b21
Will <will@pattonwebz.com>
2015-11-23 10:00:43 +0000

    add the config script needed to experiment with externalized configs 	currently only used by the add.py script

---
+++ configuration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# configuration.py
+# description: This is the config script
+# copyrigtht: 2015 William Patton - PattonWebz
+# licence: GPLv3
+# @package: PWTwitterBot
+
+## enter the database connection information
+dbconfig = {
+  'user': 'bot-twitter',
+  'password': 'someSecurePassword',
+  'host': '127.0.0.1',
+  'database': 'twitterbot',
+  'raise_on_warnings': True,
+}
